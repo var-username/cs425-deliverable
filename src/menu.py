ca776fb7336272584175825f1c3fba6a841d48d8
Variable Username <var-username@users.noreply.github.com>
2021-12-02 20:42:27 -0600

    InsufficientPrivilege errors now caught in menu

--- src/menu.py
+++ src/menu.py
@@ -1,9 +1,13 @@
+import psycopg2.errors as p2err
 import psycopg2.sql as sql
 
 from src import connection
 
 
 def main_menu(conf: dict, conn: connection.Connection) -> bool:
+    if conn._conn.Error == p2err.InFailedSqlTransaction:
+        conn.rollback()
+
     role = conn.get_user_highest_role(conf["user"])
 
     try:
@@ -22,39 +26,6 @@ def main_menu(conf: dict, conn: connection.Connection) -> bool:
         conn.commit()
         return False
 
-    # Add new item
-    # > a [?, d, o, p]
-    elif command[0].startswith('a'):
-
-        if (command[1].startswith('?')):
-            print("Usage: a [item] ...")
-
-        elif (command[1].startswith('d')):
-            # Add new donor
-            raise NotImplementedError
-
-        elif (command[1].startswith('o')):
-            # Add new organ
-            raise NotImplementedError
-
-        elif (command[1].startswith('p')):
-            # Add new patient
-            raise NotImplementedError
-
-    # Create something (admin)
-    # > c [?, u [name group (args)] ]
-    elif command[0].startswith('c'):
-
-        if (command[1].startswith('?') or len(command) < 2):
-            print("Usage: c [item] ...")
-
-        elif command[1].startswith('u'):
-            if (command[2].startswith('?') or len(command) < 4):
-                print("Usage: c u [username] [group] (args)")
-            else:
-                conn.create_user(*command[2:])
-                conn.commit()
-
     # Commit anything that hasn't been commited yet
     # In theory this shouldn't need to be used
     # > C
@@ -67,6 +38,53 @@ def main_menu(conf: dict, conn: connection.Connection) -> bool:
     elif command[0].startswith('R'):
         conn.rollback()
 
+    else:
+        # All the functions that could error because of permissions errors
+        try:
+            # Create something (admin)
+            # > c [?, u [name group (args)] ]
+            if command[0].startswith('c'):
+                if (command[1].startswith('?') or len(command) < 2):
+                    print("Usage: c [item] ...")
+
+                elif command[1].startswith('u'):
+                    if (command[2].startswith('?') or len(command) < 4):
+                       print("Usage: c u [username] [group] (args)")
+                    else:
+                       conn.create_user(*command[2:])
+                       conn.commit()
+
+            # Add new item
+            # > a [?, d, o, p]
+            elif command[0].startswith('a'):
+
+                if (command[1].startswith('?')):
+                    print("Usage: a [item] ...")
+
+                elif (command[1].startswith('d')):
+                    # Add new donor
+                    raise NotImplementedError
+
+                elif (command[1].startswith('o')):
+                    # Add new organ
+                    raise NotImplementedError
+
+                elif (command[1].startswith('p')):
+                    # Add new patient
+                    raise NotImplementedError
+
+            # Income report
+            # > I
+            elif command[0].startswith('I'):
+                raise NotImplementedError
+            
+        # NOTE: pylance claims this isn't allowed but it seems to work when I run it
+        except p2err.InsufficientPrivilege as e:
+            print(e.pgerror)
+            print("Your transaction has automatically been canceled back.")
+            conn._conn.cancel()
+            conn.rollback()
+
     return True
 
 
@@ -77,15 +95,16 @@ def print_help(role: str):
         print("  (u)ser")
         print("")
         pass
-    elif (role == 'doctor' or role == 'admin'):
+    if (role == 'doctor' or role == 'admin'):
         # Doctor actions
         print("(a)dd new")
         print("  (d)onor")
         print("  (o)rgan")
         print("  (p)atient")
+        print("(I)ncome report")
         print("")
         pass
-    elif(role == 'patient' or role == 'doctor' or role == 'admin'):
+    if(role == 'patient' or role == 'doctor' or role == 'admin'):
         # Patient actions
         print("(C)ommit (debug command)")
         print("(R)evert (debug command)")
